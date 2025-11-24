960292e4b49816fa503fa02e53bb69f9b6b0c762
Md Sayedul Haque <takisayedul@gmail.com>
2025-11-24 12:41:10 +0600

    Add: task_statuses table

---
+++ services/hr-api/src/modules/task_statuses/task_statuses.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
+import { TaskStatusesService } from './task_statuses.service';
+import { CreateTaskStatusDto } from './dto/create.dto';
+import { UpdateTaskStatusDto } from './dto/update.dto';
+
+@ApiTags('TaskStatuses')
+@Controller('task-statuses')
+export class TaskStatusesController {
+  constructor(private readonly taskStatusesService: TaskStatusesService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create task status' })
+  @ApiBody({ type: CreateTaskStatusDto })
+  @ApiResponse({ status: 201, description: 'Task status created' })
+  create(@Body() createDto: CreateTaskStatusDto) {
+    return this.taskStatusesService.create(createDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all task statuses' })
+  @ApiResponse({ status: 200, description: 'List of task statuses' })
+  findAll() {
+    return this.taskStatusesService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get task status by id' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Task status found' })
+  findOne(@Param('id') id: number) {
+    return this.taskStatusesService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update task status by id' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiBody({ type: UpdateTaskStatusDto })
+  @ApiResponse({ status: 200, description: 'Task status updated' })
+  update(@Param('id') id: number, @Body() updateDto: UpdateTaskStatusDto) {
+    return this.taskStatusesService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete task status by id' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Task status deleted' })
+  remove(@Param('id') id: number) {
+    return this.taskStatusesService.remove(id);
+  }
+}
--- services/hr-api/src/modules/tasks/tasks.module.ts
+++ services/hr-api/src/modules/tasks/tasks.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tasks } from '../../models/tasks.entity';
 import { Employees } from '../../models/employees.entity';
+import { TaskStatuses } from '../../models/task_statuses.entity';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tasks, Employees])],
+  imports: [TypeOrmModule.forFeature([Tasks, Employees, TaskStatuses])],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [TasksService],
