979865bdfcc5cb96fe1b20ac8ab60878cff40bf2
nuralam24 <nuralamcse024@gmail.com>
2025-11-26 14:57:53 +0600

    Work on emp_attendance & departments

--- services/hr-api/src/modules/emp_attendances/emp_attendances.controller.ts
+++ services/hr-api/src/modules/emp_attendances/emp_attendances.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Param, Put, Delete, Patch } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
+import { Controller, Get, Post, Body, Param, Put, Delete, Patch, Query, HttpStatus, Res } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam, ApiQuery } from '@nestjs/swagger';
 import { CreateEmpAttendanceDto } from './dto/create.dto';
 import { UpdateEmpAttendanceDto } from './dto/update.dto';
 import { EmpAttendancesService } from './emp_attendances.service';
 import { EmployeesService } from '../employees/employees.service';
+import { Response } from 'express';
+
 
 @ApiTags('EmployeeAttendances')
 @Controller('emp-attendances')
@@ -22,8 +24,22 @@ export class EmpAttendancesController {
   @Get()
   @ApiOperation({ summary: 'Get all employee attendances' })
   @ApiResponse({ status: 200, description: 'List of attendances' })
-  findAll() {
-    return this.empAttendancesService.findAll();
+  @ApiQuery({ name: 'page', type: Number, required: false, example: 1 })
+  @ApiQuery({ name: 'limit', type: Number, required: false, example: 10 })
+  @ApiQuery({ name: 'sortBy', type: String, required: false, enum: ['createdAt', 'updatedAt'], example: 'createdAt' })
+  @ApiQuery({ name: 'sortOrder', type: String, required: false, enum: ['asc', 'desc'], example: 'asc' })
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('sortBy') sortBy: string = 'createdAt',
+    @Query('sortOrder') sortOrder: string = 'asc',
+    @Res() res: Response  
+  ) {
+    const response = await this.empAttendancesService.findAll(page, limit, sortBy, sortOrder);
+    return res.status(HttpStatus.OK).json({ 
+      success: true, data: response.data, metaData: response.metaData,
+      message: response.data.length > 0 ? 'Attendances fetched successfully!' : 'No attendances found!' 
+    });
   }
 
   @Get(':id')
