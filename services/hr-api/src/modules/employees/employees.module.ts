727cd77912785cf399371f6d99f659b0a6017c1a
nuralam24 <nuralamcse024@gmail.com>
2025-12-02 16:37:00 +0600

    updaet new api for employee profile view & update

--- services/hr-api/src/modules/employees/employees.module.ts
+++ services/hr-api/src/modules/employees/employees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employees } from '../../models/employees.entity';
 import { EmployeesService } from './employees.service';
@@ -6,9 +6,16 @@ import { EmployeesController } from './employees.controller';
 import { EmpDepartmentsModule } from '../emp_departments/emp_departments.module';
 import { EmpJobTitlesModule } from '../emp_job_titles/emp_job_titles.module';
 import { MailModule } from '../email/mail.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employees]), EmpDepartmentsModule, EmpJobTitlesModule, MailModule],
+  imports: [
+    TypeOrmModule.forFeature([Employees]), 
+    EmpDepartmentsModule, 
+    EmpJobTitlesModule, 
+    MailModule,
+    forwardRef(() => UsersModule)
+  ],
   controllers: [EmployeesController],
   providers: [EmployeesService],
   exports: [EmployeesService],
