b006b9e5981545f14d88b2e6442a5e4991487b40
niloydiu <niloy15-13991@diu.edu.bd>
2025-12-02 12:28:47 +0600

    Fix formatting of image export paths in images.js

--- web/hr-admin/lib/images.js
+++ web/hr-admin/lib/images.js
@@ -1,7 +1,7 @@
 // Image exports for scalable asset management
 // All public images are exported here for easy importing and usage across the app
 
-export const aftabLogo = '/aftab-logo.png';
+export const aftabLogo = "/aftab-logo.png";
 
 // Add more image exports here as needed, e.g.:
 // export const userAvatar = '/user-avatar.png';
